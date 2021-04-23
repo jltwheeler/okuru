c4bd438703815001d37ccaa10ca4d0ef61703ab4
jltwheeler <jltwheeler@gmail.com>
2021-04-23 21:33:18 +0100

    add refresh token fetch to main component

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
 import { ApolloProvider } from "@apollo/client/react";
@@ -5,11 +6,13 @@ import { setContext } from "@apollo/client/link/context";
 
 import theme from "../theme";
 import { AppProps } from "next/app";
-import { getAccessToken } from "src/utils/accessToken";
+import { getAccessToken, setAccessToken } from "src/utils/accessToken";
 import { __prod__ } from "src/constants";
+import { useState } from "react";
 
 const httpLink = createHttpLink({
   uri: __prod__ ? "" : "http://localhost:5000/graphql",
+  credentials: "include",
 });
 
 const authLink = setContext((_, { headers }) => {
@@ -22,13 +25,31 @@ const authLink = setContext((_, { headers }) => {
 });
 
 const client = new ApolloClient({
-  uri: "",
   cache: new InMemoryCache(),
-  credentials: "include",
   link: authLink.concat(httpLink),
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/refresh_token", {
+      method: "POST",
+      credentials: "include",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setAccessToken(data.access_token);
+        console.log(data);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    // Make this some nice loading animation
+    return <div> LOADING ...</div>;
+  }
+
   return (
     <ApolloProvider client={client}>
       <ChakraProvider resetCSS theme={theme}>
